4effd5eeddde66308bad8549754b97e855ee8e5f
d.vladimirov <d.vladimirov@ritm.media>
2025-09-05 12:58:51 +0300

    vers_1_1 added conftest.py, modified main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-from selenium import webdriver
+import pytest
 from selenium.webdriver.common.by import By
 import time
 
-driver_path = '/usr/local/bin/chromedriver'
 url = 'https://google.com'
 
-driver = webdriver.Chrome()
+
+@pytest.fixture
+def open_form(browser):
+    browser.get(url)
+    yield
+    browser.quit()
 
 
-def test_google_title():
+def test_google_title(browser, open_form):
     try:
-        driver.get(url)
-        time.sleep(5)
-
-        title = driver.title
+        title = browser.title
         print(f"Заголовок страницы: {title}")
         expected_title = "Google"
         assert title == expected_title, f"Заголовок не соответствует: ожидаемый '{expected_title}', полученный '{title}'"
@@ -23,25 +24,16 @@ def test_google_title():
     except AssertionError as e:
         print(e)
 
-    finally:
-        driver.quit()
 
-
-def test_google_search():
+def test_google_search(browser, open_form):
     try:
-        driver.get(url)
-        time.sleep(2)
-
-        elements = driver.find_elements(By.CSS_SELECTOR, "textarea[class='gLFyf']")
+        elements = browser.find_elements(By.CSS_SELECTOR, "textarea[class='gLFyf']")
         for element in elements:
             element.send_keys('Погода в Москве')
         time.sleep(1)
-        button = driver.find_element(By.CSS_SELECTOR, "input.gNO89b")
+        button = browser.find_element(By.CSS_SELECTOR, "input.gNO89b")
         button.click()
         time.sleep(2)
         print("Тест пройден успешно!")
     except AssertionError as e:
         print(e)
-
-    finally:
-        driver.quit()
